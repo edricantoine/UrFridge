454ebdd422d7e6bedbf5733ec2d0436fdf956bd6
Edric Antoine <edric.antoine@gmail.com>
2022-04-10 21:13:05 -0700

    Implemented recipe grabber class

--- UI/main.py
+++ UI/main.py
@@ -5,11 +5,25 @@ from kivy.uix.label import Label
 
 
 class Main(App):
+    # all possible meal types
+    mealTypes = ["All", "Breakfast", "Lunch", "Dinner", "Snack", "Teatime"]
+    # all possible dish types
+    dishTypes = ["All", "Alcohol-cocktail", "Biscuits and cookies", "Bread", "Cereals", "Condiments and sauces",
+                 "Drinks",
+                 "Desserts", "Egg", "Main course", "Omelet", "Pancake", "Preps", "Preserve", "Salad", "Sandwiches",
+                 "Soup", "Starter"]
+    # all possible regional cuisine types
+    cuisineTypes = ["All", "American", "Asian", "British", "Caribbean", "Central Europe", "Chinese", "Eastern Europe",
+                    "French", "Indian", "Italian", "Japanese", "Kosher", "Mediterranean", "Mexican", "Middle Eastern",
+                    "Nordic", "South American", "South East Asian"]
+
+    grb = None
+
     def build(self):
         label = Label(text="Testing",
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
-        grb = Grab.RecipeGrabber(["cheese", "bacon"], 1000)
+        self.grb = Grab.RecipeGrabber()
 
         return label
 
