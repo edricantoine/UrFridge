366403466226166b6e150b247309119989d256f2
Edric Antoine <edric.antoine@gmail.com>
2022-04-21 15:58:38 -0700

    Create fridge screen in progress

--- UI/main.py
+++ UI/main.py
@@ -1,17 +1,55 @@
 from kivy.app import App
 from kivy.config import Config
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+import Backend.Application as Apple
 
 
-class myLayout(FloatLayout):
+class MainScreen(Screen):
+    app = None
+
+    def __init__(self, aap: Apple.Application, **kw):
+        super().__init__(**kw)
+        self.app = aap
+
+    def printFridges(self):
+        for f in self.app.getFridges():
+            print(f.getName())
+
+
+class createFridgeScreen(Screen):
+    app = None
+
+    def attemptAddFridge(self, name):
+        if self.app.addFridge(name):
+            return "Fridge successfully added!"
+        else:
+            return "Invalid fridge name."
+
+    def __init__(self, aap: Apple.Application, **kw):
+        super().__init__(**kw)
+        self.app = aap
+
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class selectFridgeScreen(Screen):
     pass
 
 
 class Main(App):
-    grb = None
+    app = None
 
     def build(self):
-        return myLayout()
+        self.app = Apple.Application()
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(self.app, name="Main Screen"))
+        sm.add_widget(createFridgeScreen(self.app, name="Create Fridge"))
+        return sm
 
 
 if __name__ == '__main__':
