60ae856441b4ed82bddda8ff3f5a029eb36e4c31
Edric Antoine <edric.antoine@gmail.com>
2022-06-22 16:56:44 -0700

    How tf is this working???

--- ui/LoadingScreen.py
+++ ui/LoadingScreen.py
@@ -8,6 +8,6 @@ class LoadingScreen(Screen):
         super().__init__(**kw)
 
     def on_enter(self):
-        self.app.aap.getRecipeFromSelectedIngredients(app.numToGrab, None)
-        self.app.sm.get_screen("Recipe List").initializeFromRecipesList(app.aap.getRecipes())
+        self.app.app.getRecipeFromSelectedIngredients(self.app.numToGrab, None)
+        self.app.sm.get_screen("Recipe List").initializeFromRecipesList(self.app.app.getRecipes())
         self.app.sm.current = 'Recipe List'
