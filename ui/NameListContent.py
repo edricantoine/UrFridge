d0e72e5dd549fbd606998b7ab481ce06c6f0160b
Edric Antoine <edric.antoine@gmail.com>
2022-06-22 19:13:20 -0700

    Fixed bug in saving list

--- ui/NameListContent.py
+++ ui/NameListContent.py
@@ -18,22 +18,24 @@ class NameListContent(BoxLayout):
 
     def readInput(self):
         liste = Lis.IngList()
-        for i in self.app.aap.getSelectedIngredients():
+        for i in self.app.app.getSelectedIngredients():
             liste.addIngredient(i)
         name = self.ids.listName.text
-        if self.app.aap.setListName(liste, name):
-            self.app.aap.addListToLists(liste)
+        if self.app.app.setListName(liste, name):
+            self.app.app.addListToLists(liste)
             listeNames = []
             for i in liste.main_list:
                 listeNames.append(i.getName())
             self.c.execute("""
                         INSERT INTO lists(name, contents, owner)
                         VALUES(?, ?, ?)
-                        """, (name, json.dumps(listeNames), self.app.aap.getId()))
+                        """, (name, json.dumps(listeNames), self.app.app.getId()))
             self.squirrel.commit()
             toast("List added successfully!")
         else:
             toast("You already have a list with that name.")
 
         self.ids.listName.text = ""
+        for l in self.app.app.getLists():
+            print(l.getName())
 
